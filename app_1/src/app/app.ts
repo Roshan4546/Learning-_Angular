15b29ee2b3f03cf5f2a2f342d23936d8282c017e
Roshan4546 <roshanpatra275@gmail.com>
2025-08-09 18:18:34 +0530

    changes in code

--- app_1/src/app/app.ts
+++ app_1/src/app/app.ts
@@ -1,14 +1,37 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, Login,Signup],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
+
+  
   title = 'App_1';
   name = 'Roshan';
-  
+  // x: boolean = false;
+
+  // // setXTrue() {
+  // //   this.x = true;
+  // // }
+  // changehandler() {
+  //   this.x = true;
+  // }
+  // items: string[][] = [
+  //   ['id', 'name', 'age', 'sec'],
+  //   ['1', 'Roshan', '20', 'a'],
+  //   ['2', 'Tabhis', '21', 'b'],
+  //   ['3', 'Sachin', '22', 'c'],
+  //   ['4', 'Ayush', '20', 'c']
+  // ];
+
+
+
 }
